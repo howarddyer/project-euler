e742c93ad7a6c72a55c9c2dc77cc3fe39c402d9b
howarddyer <me@howard-dyer.co.uk>
2017-05-17 23:18:10 +0100

    Add new attempt for 001 Multiples of 3 and 5.

---
+++ c/001-multiples-of-3-and-5/1.c
@@ -0,0 +1,25 @@
+#import <stdio.h>
+#include "../output.c"
+
+/*
+  Go through numbers 1 to 1000
+  Sum those divisable by 3
+*/
+
+int main () {
+  output(solution);
+}
+
+int solution() {
+  int i;
+  int sum = 0;
+
+
+  for (i = 1; i < 1000; i++) {
+    if (i % 3 == 0 ) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}

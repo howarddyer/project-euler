461a6d95b3082b37edb5088c6d8e36e14dbb9737
howarddyer <me@howard-dyer.co.uk>
2017-05-24 18:23:04 +0100

    Add 2nd attempt for 001 Multiples of 3 and 5.

---
+++ c/001-multiples-of-3-and-5/2.c
@@ -0,0 +1,32 @@
+#import <stdio.h>
+#import <math.h>
+#include "../output.c"
+
+/*
+  Get all numbers divisable by 3 or 5 using the triangular number formula — (d * 0.5) * (d + 1)
+  Sum those divisable by 3 or 5
+*/
+
+int main () {
+  output(solution);
+}
+
+int solution() {
+  int sum = 0;
+
+  sum = divisableBy(3) + divisableBy(5) - divisableBy(15);
+
+  return sum;
+
+}
+
+int divisableBy(int divider) {
+  int target = 999,
+      upper;
+
+  upper = floor(target / divider);
+
+  // Triangular number formula
+  return divider * ( (upper * 0.5) * (upper + 1) );
+
+}
